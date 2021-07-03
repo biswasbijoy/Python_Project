3a7aa46cf8512dc26b7a217e312347c15b302495
Bijoy Biswas <biswasbijoy414@gmail.com>
2021-07-03 23:57:23 +0600

    text in tkinter

---
+++ GUI/Tkinter/Textinturtle.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+
+
+root = Tk()
+
+lbl = Label(root, text = "This is my first program using tkinter in python",
+            font = ('Harlow Solid Italic', 18, 'bold'))
+lbl.pack(padx = 10, pady = 235)
+
+
+
+
+root.mainloop()
